2d914caea89d861e7fc06d1aac9736a115e8d371
anokhimatta <143225653+anokhimatta@users.noreply.github.com>
2025-03-29 19:13:47 -0400

    Added Leasing Office model

--- apartment-hunter/backend/models.py
+++ apartment-hunter/backend/models.py
@@ -2,6 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class LeasingOffice(db.Model):
+    __table_name__ = "Leasing Office"
+    office_id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
+    address = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    phone_number = db.Column(db.Integer, unique=True, nullable=False)
+    open_hour = db.Column(db.Integer, nullable=False)
+    close_hour = db.Column(db.Integer, nullable=False)
+
 class LeaseApplications(db.Model):
     __table_name__ = "Lease Applications"
     application_id = db.Column("application_id", db.Integer, primary_key=True, unique=True, nullable=False)
@@ -9,12 +19,23 @@ class LeaseApplications(db.Model):
     property_id = db.Column("app_property_id", db.Integer, db.ForeignKey('Property.property_id'), nullable=False)
     status = db.Column(db.String, nullable=False)
 
-    # Relationships (Optional, if you want to access related data easily)
     renter = db.relationship("Renter", backref="applications")
     property = db.relationship("Property", backref="applications")
 
 
 
+
+
+# CREATE TABLE "Leasing Office" (
+#	"office_id"	INTEGER NOT NULL UNIQUE,
+#	"name"	TEXT NOT NULL UNIQUE,
+#	"address"	TEXT NOT NULL UNIQUE,
+#	"email"	TEXT NOT NULL UNIQUE,
+#	"phone_number"	INTEGER NOT NULL UNIQUE,
+#	"open_hour"	INTEGER NOT NULL,
+#	"close_hour"	INTEGER NOT NULL,
+#	PRIMARY KEY("office_id")
+#)
 
 # CREATE TABLE "Lease Applications" (
 #	"application_id"	INTEGER NOT NULL UNIQUE,
