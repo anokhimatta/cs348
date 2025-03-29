70c52e861d7c4d58eaf11ba360e64c69d7154e43
anokhimatta <143225653+anokhimatta@users.noreply.github.com>
2025-03-29 18:25:51 -0400

    apartments.db integration

--- apartment-hunter/backend/app.py
+++ apartment-hunter/backend/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(BASE_DIR, 'apartments.db')}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
