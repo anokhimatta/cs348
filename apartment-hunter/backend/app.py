035558eb0ed4c627568f06b694212376c2484df8
anokhimatta <143225653+anokhimatta@users.noreply.github.com>
2025-03-29 18:35:37 -0400

    models.py created, app.py fixed for db

--- apartment-hunter/backend/app.py
+++ apartment-hunter/backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models import db
+#from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
@@ -8,4 +9,10 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(BASE_DIR, 'apartments.db')}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
