43570d198650be8a9fb53fa66bcf0d8828ab04c5
anokhimatta <143225653+anokhimatta@users.noreply.github.com>
2025-03-31 02:07:36 -0400

    literally deleted everything bc nothing works

---
+++ app.py
@@ -0,0 +1,8 @@
+import sqlite3
+from flask import Flask, request, jsonify, g
+from flask_cors import CORS
+import os
+app = Flask(__name__)
+CORS(app)
+
+DATABASE = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'temp.db')
