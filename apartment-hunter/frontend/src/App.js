831471d626f7a7fba1f916c13e9f21c79b4c335e
anokhimatta <143225653+anokhimatta@users.noreply.github.com>
2025-05-01 23:41:27 -0400

    removed LeasingOffice and Reviews

--- apartment-hunter/frontend/src/App.js
+++ apartment-hunter/frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
 import RenterList from './RenterList.js';
 import RenterForm from './RenterForm.js';
 import RenterDetail from './RenterDetail.js';
 import EditRenterForm from './EditRenterForm.js';
+import Property from './Property.js';
 import './App.css';
 
 function App() {
@@ -14,8 +15,9 @@ function App() {
           <h1>Apartment Rental Management</h1>
           <nav className="main-nav">
             <ul>
-              <li><a href="/">Renters</a></li>
-              {/* Add other nav links for Properties, Offices, etc. */}
+              <li><Link to="/renters">Renters</Link></li>
+              <li><Link to="/properties">Properties</Link></li>
+              {/* Add other nav links for Offices, etc. */}
             </ul>
           </nav>
         </header>
@@ -27,6 +29,13 @@ function App() {
             <Route path="/renters/new" element={<RenterForm />} />
             <Route path="/renters/:id" element={<RenterDetail />} />
             <Route path="/renters/:id/edit" element={<EditRenterForm />} />
+            
+            {/* Add routes for Properties */}
+            <Route path="/properties" element={<Property />} />
+            {/* If you have separate components for property details, add them here */}
+            {/* <Route path="/properties/new" element={<PropertyForm />} /> */}
+            {/* <Route path="/properties/:id" element={<PropertyDetail />} /> */}
+            {/* <Route path="/properties/:id/edit" element={<EditPropertyForm />} /> */}
           </Routes>
         </main>
         
